056c433a29f5c29fb8e9fecdfc0dcf9eda2c341e
Your Name <you@example.com>
2024-03-01 18:02:47 +0530

    Changes Updated

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,38 +1,35 @@
 import React, { useState } from "react"
 import "./Todo.css"
 export default function Todo() {
-  const [todo, setTodo] = useState("")
-  const [array, setArray] = useState([] as any)
+    const [todo, setTodo] = useState<string>("")
+    const [array, setArray] = useState<{ todo: string; checked: boolean }[]>([])
 
-  const handleAdd = () => {
-    setArray([...array, { todo: todo }])
-    setTodo('')
-  }
-  const handleDelete = (index: any) => {
-    // const newTodos=[...array]
-    // newTodos.splice(1)
-    // setArray(newTodos)
-    const updatedTodos = array.filter((_: any, i: any) => i !== index);
-    setArray(updatedTodos);
-  }
-  const handleCheckbox = (index: any) => {
-    const updatedTodos = [...array];
-    updatedTodos[index] = !updatedTodos[index];
-    setArray(updatedTodos);
-  }
-  return (
-    <div className="container">
-        <h1 className="header">Todo List</h1>
-        <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
-        <button className="btn-Add" onClick={handleAdd}>Add</button>
-        <div style={{ marker: "none" }}>
-          {array.map((item: any, index: number) => <div key={index} className="inputfield">
-            {index+1}.
-            {item.todo}
-            <input type="checkbox" onChange={(index) => handleCheckbox(index)} style={{ height: "22px", width: "25px", marginRight: "5px" ,marginLeft:"10px"}} />
-            <button className="btn-delete" onClick={() => handleDelete(index)}>Delete</button>
-          </div>)}
+    const handleAdd = () => {
+        setArray([...array, { todo, checked: false }])
+        setTodo('')
+    }
+    const handleDelete = (index: number) => {
+        const updatedTodos = array.filter((_: object, i: number) => i !== index);
+        setArray(updatedTodos);
+    }
+    const handleCheckbox = (index: number) => {
+        const updatedTodos = [...array];
+        updatedTodos[index].checked = !updatedTodos[index].checked;
+        setArray(updatedTodos);
+    }
+    return (
+        <div className="container">
+            <h1 className="header">Todo List</h1>
+            <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
+            <button className="btn-Add" onClick={handleAdd}>Add</button>
+            <div style={{ marker: "none" }}>
+                {array.map((item: any, index: number) => <div key={index} className="inputfield">
+                    {index + 1}.
+                    {item.todo}
+                    <input type="checkbox" className="check-box" onChange={() => handleCheckbox(index)} />
+                    <button className="btn-delete" onClick={() => handleDelete(index)}>Delete</button>
+                </div>)}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
