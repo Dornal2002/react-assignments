871a148b8e719e78de69e2e028d5faa253a4945c
Your Name <you@example.com>
2024-03-04 18:01:37 +0530

    changes

--- src/components/GetTodos.tsx
+++ src/components/GetTodos.tsx
@@ -1,15 +1,21 @@
-const GetTodos=(props:any)=>{
-    return(
-        <div>
-        {
-            props.todos.map((data:any)=>(
-                <div key={data.id}>
-                <h4>{data.todo}</h4>
-                </div>
-            ))
-        }
-        </div>
-    )
-
+export interface Todo {
+  id: number;
+  todo: string;
 }
+export interface GetTodosProps {
+  todos: Todo[];
+}
+
+const GetTodos = (props: GetTodosProps) => {
+  const { todos } = props;
+  return (
+    <div>
+      {todos.map((data: any) => (
+        <div key={data.id}>
+          <h4>{data.todo}</h4>
+        </div>
+      ))}
+    </div>
+  );
+};
 export default GetTodos;
