2d82a4adb9eedd4b1518adbbbb153ffc1bf7df15
Your Name <you@example.com>
2024-03-06 12:41:37 +0530

    Assignment 3

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
 import Todos from './components/Todos';
+import Blogs from './components/Blogs';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Todos/>}/>
+          <Route path='/blogs' element={<Blogs/>}/>
         </Routes>
       </Router>
     </div>
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import 'bootstrap/dist/css/bootstrap.css';
 
 export interface Todo{
     id:number
@@ -9,15 +10,24 @@ export interface Todo{
 export default function Todos(){
 
     const [todolist,setTodolist]=useState<Todo[]>([])
-    const [newTodo,setNewTodo]=useState<Todo>()
+    // const [newTodo,setNewTodo]=useState<Todo>()
 
     useEffect(()=>{
-        fetch("")
-        .then()
+        fetch("http://localhost:8000/todos")
+        .then(resp=>resp.json())
+        .then(data=>(setTodolist(data)))
     },[])
     return(
-        <div>
+       <>
+        <div className="container">
+        {
+            todolist.map((item)=><div key={item.id} className="card">
+                <h1 className="card-body">{item.title}</h1>
+                <input type="checkbox" checked={item.isCompleted} />
+            </div>)
+        }
            
         </div>
+        </>
     )
 }
