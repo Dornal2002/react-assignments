65e55ddf220a0ec26a8348936f29e3f36231b24f
Your Name <you@example.com>
2024-03-14 12:54:38 +0530

    Assignment 7 Added

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -7,7 +7,6 @@ import useFetch from "../hooks/useFetch";
 import { useQuery, useMutation } from "react-query";
 import axios from "axios";
 import { Todo } from "../types/Todo";
-import { string } from "yargs";
 
 export default function Todos() {
   // let { data, isLoading, error,refetch } = useFetch("http://localhost:8000/todos");
@@ -15,7 +14,7 @@ export default function Todos() {
   const [search, setSearch] = useState<string>("");
   const [status, setStatus] = useState("all");
   const [page, setPage] = useState<number>(1);
-  const [limit, setLimit] = useState<number>(3);
+  const [limit, setLimit] = useState<number>(2);
   const [totalpages, setTotalPages] = useState(2);
   const [sortBy, setSortBy] = useState<string>("name");
   const [sortOrder, setSortOrder] = useState<string>("asc");
@@ -23,11 +22,12 @@ export default function Todos() {
     queryKey: ["todos", page, limit, search, sortBy, sortOrder],
     queryFn: async () => {
       const res = await axios.get(
-        // `http://localhost:8000/todos?_page=${page}&_per_page=${limit}&_search=${search}`
-        `http://localhost:8000/todos?_page=${page}&_per_page=${limit}`
+        // `http://localhost:8000/todos?_page=${page}&_per_page=${limit}&`
+        `http://localhost:8000/todos?_page=${page}&_limit=${limit}&title_like=${search}&_sort=${sortBy}&_order=${sortOrder}&isCompleted_like=${status}`
       );
-      setTotalPages(res.data.last);
-      return res.data.data;
+      setTotalPages(Math.ceil(res.headers['x-total-count']/limit))
+      if(!limit) setTotalPages(1)
+      return res.data;
     },
   });
 
@@ -60,42 +60,42 @@ export default function Todos() {
     }
   );
 
-  const handleStatus = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    setStatus(e.currentTarget.value);
-  };
+  // const handleStatus = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  //   setStatus(e.currentTarget.value);
+  // };
 
-  const filteredData = todolist?.filter((item: Todo) => {
-    if (status === "all") {
-      return true;
-    } else if (status === "complete") {
-      return item.isCompleted === true;
-    } else if (status === "incomplete") {
-      return item.isCompleted === false;
-    }
-    return false;
-  });
+  // const filteredData = todolist?.filter((item: Todo) => {
+  //   if (status === "all") {
+  //     return true;
+  //   } else if (status === "complete") {
+  //     return item.isCompleted === true;
+  //   } else if (status === "incomplete") {
+  //     return item.isCompleted === false;
+  //   }
+  //   return false;
+  // });
 
-  const handleSort = (criteria: string) => {
-    if (criteria === sortBy) {
-      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
-    } else {
-      setSortBy(criteria);
-      setSortOrder("asc");
-    }
-  };
+  // const handleSort = (criteria: string) => {
+  //   if (criteria === sortBy) {
+  //     setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  //   } else {
+  //     setSortBy(criteria);
+  //     setSortOrder("asc");
+  //   }
+  // };
 
-  filteredData?.sort((a: Todo, b: Todo) => {
-    if (sortBy === "name") {
-      return sortOrder === "asc"
-        ? a.title.localeCompare(b.title)
-        : b.title.localeCompare(a.title);
-    } else if (sortBy === "date") {
-      return sortOrder === "asc"
-        ? new Date(a.date).getTime() - new Date(b.date).getTime()
-        : new Date(b.date).getTime() - new Date(a.date).getTime();
-    }
-    return 0;
-  });
+  // filteredData?.sort((a: Todo, b: Todo) => {
+  //   if (sortBy === "name") {
+  //     return sortOrder === "asc"
+  //       ? a.title.localeCompare(b.title)
+  //       : b.title.localeCompare(a.title);
+  //   } else if (sortBy === "date") {
+  //     return sortOrder === "asc"
+  //       ? new Date(a.date).getTime() - new Date(b.date).getTime()
+  //       : new Date(b.date).getTime() - new Date(a.date).getTime();
+  //   }
+  //   return 0;
+  // });
 
   if (error) {
     alert(error);
@@ -133,7 +133,7 @@ export default function Todos() {
                   className={`dropdown-item ${
                     sortBy === "name" ? "active" : ""
                   }`}
-                  onClick={() => handleSort("name")}
+                  onClick={() => setSortBy("name")}
                 >
                   Name
                 </button>
@@ -141,15 +141,18 @@ export default function Todos() {
                   className={`dropdown-item ${
                     sortBy === "date" ? "active" : ""
                   }`}
-                  onClick={() => handleSort("date")}
+                  onClick={() => setSortBy("date")}
                 >
                   Date
                 </button>
               </div>
             </div>
             <div>
-              <span onClick={() => handleSort(sortBy)}>
-                {sortOrder === "asc" ? <ArrowUp /> : <ArrowDown />}
+              <span onClick={() => setSortOrder("asc")}>
+                 <ArrowUp />
+              </span>
+              <span onClick={() => setSortOrder("desc")}>
+                <ArrowDown />
               </span>
             </div>
             <div className="dropdown ms-3">
@@ -170,21 +173,24 @@ export default function Todos() {
                 <button
                   className="dropdown-item"
                   value="all"
-                  onClick={handleStatus}
+                  // onClick={handleStatus}
+                  onClick={()=>(setStatus("all"))}
                 >
                   All
                 </button>
                 <button
                   className="dropdown-item"
                   value="complete"
-                  onClick={handleStatus}
+                  // onClick={handleStatus}
+                  onClick={()=>(setStatus("complete"))}
                 >
                   Completed
                 </button>
                 <button
                   className="dropdown-item"
                   value="incomplete"
-                  onClick={handleStatus}
+                  // onClick={handleStatus}
+                  onClick={()=>(setStatus("incomplete"))}
                 >
                   Incomplete
                 </button>
@@ -202,12 +208,7 @@ export default function Todos() {
                 </tr>
               </thead>
               <tbody>
-                {filteredData
-                  ?.filter((item) => {
-                    return search.toLocaleUpperCase() === ""
-                      ? item
-                      : item.title.toLocaleLowerCase().includes(search);
-                  })
+                {todolist
                   .map((item: Todo, index: number) => (
                     <tr key={item.id}>
                       <td>{index + 1}</td>
